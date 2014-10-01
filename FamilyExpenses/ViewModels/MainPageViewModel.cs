345abee2d4a3c9896e43082d992a203938ef8b7f
antofik <antofik@gmail.com>
2014-10-01 10:45:34 +0400

    initial

---
+++ FamilyExpenses/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+
+namespace FamilyExpenses.ViewModels
+{
+    public class MainPageViewModel : INotifyPropertyChanged
+    {
+        public MainPageViewModel()
+        {
+#if DEBUG
+            const string names = "Дети;Машина;Личное;Другое";
+            foreach(var name in names.Split(new[]{';'}))
+                Categories.Add(new Category(name));
+#endif
+        }
+
+        #region Categories
+
+        private ObservableCollection<Category> _categories = new ObservableCollection<Category>();
+
+        public ObservableCollection<Category> Categories
+        {
+            get { return _categories; }
+            set
+            {
+                if (_categories == value) return;
+                _categories = value;
+                OnPropertyChanged();
+            }
+        }
+
+        #endregion
+
+        #region INotifyPropertyChanged
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        #endregion
+    }
+
+    [DataContract]
+    public class Category
+    {
+        public Category(string name)
+        {
+            Name = name;
+        }
+
+        [DataMember]
+        public string Name { get; set; }
+
+        [DataMember]
+        public int UsageCount { get; set; }
+    }
+}
